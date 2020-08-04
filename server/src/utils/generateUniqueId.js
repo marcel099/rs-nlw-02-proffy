9803c4c5a1b1fecf1ea3f71144dabb7534af622a
Marcel099 <44550398+Marcel099@users.noreply.github.com>
2020-08-04 01:28:49 -0300

    Removendo o back-end de testes

--- server/src/utils/generateUniqueId.js
+++
@@ -1,9 +0,0 @@
-const crypto = require('crypto')
-
-module.exports = function generateUniqueId() {
-    return crypto.randomBytes(4).toString('HEX')
-}
-
-// Por que separar isso em outro arquivo?
-// Caso outra parte da aplicação também use a mesma funcionalidade
-//concentra-se toda a geração em um único local
