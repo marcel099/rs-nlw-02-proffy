95ec3fd78cdf2ed7ab1f6ba66a89b89c62a67c8c
Marcelo Lupatini <mar.lupatini@gmail.com>
2022-10-03 18:53:03 -0300

    feat(server): desenv. da rota back-end de cadastro de usuário
    Tive de modificar a migration 00_create_users para alterar a possibilidade de informar valores nulos
    para 3 campos em vez de criar uma migration dedicada a isso porque o Knex não suporta alteração do
    NOT NULL de uma coluna já criada.

--- server/src/database/migrations/00_create_users.ts
+++ server/src/database/migrations/00_create_users.ts
@@ -4,9 +4,9 @@ export async function up(knex: Knex) {
   return knex.schema.createTable('users', table => {
     table.increments('id').primary();
     table.string('name').notNullable();
-    table.string('avatar').notNullable();
-    table.string('whatsapp').notNullable();
-    table.string('bio').notNullable();
+    table.string('avatar').nullable();
+    table.string('whatsapp').nullable();
+    table.string('bio').nullable();
   })
 }
 
