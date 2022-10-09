11b5ddf9a5847f2f5bde0274f84d031114071863
Marcelo Lupatini <mar.lupatini@gmail.com>
2022-10-09 15:51:37 -0300

    fix(server): correções na criação de um usuário
    - Validação de e-mail informado repetido a fim de evitar que existam 2 usuários com o mesmo e-mail
    - Senha criptografada antes de salvar usuário no banco de dados

--- server/src/controllers/UsersController.ts
+++ server/src/controllers/UsersController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import { hash } from "bcrypt";
 
 import db from '../database/connection';
 
@@ -11,11 +12,24 @@ export class UsersController {
       password,
     } = request.body;
 
+    const userFound = await db('users')
+      .where('email', '=', email)
+      .select('id', 'password')
+      .first();
+
+    if (userFound) {
+      return response.status(400).json({
+        message: 'Usuário já existe'
+      })
+    }
+
+    const passwordHash = await hash(password, 8);
+
     await db('users').insert({
       first_name,
       last_name,
       email,
-      password,
+      password: passwordHash,
     })
 
     return response.status(201).json();
