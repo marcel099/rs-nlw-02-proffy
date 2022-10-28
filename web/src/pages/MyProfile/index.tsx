129179a01af1903434ce88317df1756a9ec50169
Marcelo Lupatini <mar.lupatini@gmail.com>
2022-10-27 22:01:56 -0300

    feat(web): cabeçalho da interface MyProfile

--- web/src/pages/MyProfile/index.tsx
+++ web/src/pages/MyProfile/index.tsx
@@ -1,14 +1,55 @@
+import myProfileDesktopBackgroundImg from '@assets/images/my-profile-desktop-background.svg';
+import myProfileMobileBackgroundImg from '@assets/images/my-profile-mobile-background.svg';
+
+import { useAuth } from '@contexts/AuthContext';
+
 import { PageHeader } from '@components/PageHeader';
+import { UserAvatar } from '@components/UserAvatar';
 
 import './styles.css';
 
 export function MyProfile() {
+  const { user } = useAuth();
+
+  let myProfileDesktopImg: string;
+
+  if (window.matchMedia('(min-width: 700px)').matches) {
+    myProfileDesktopImg = myProfileDesktopBackgroundImg;
+  } else {
+    myProfileDesktopImg = myProfileMobileBackgroundImg;
+  }
+
+  function handleUserAvatarChange() {
+    console.log('a');
+  }
+
   return (
     <div id="page-my-profile" className="page-container">
       <PageHeader
         title="Meu perfil"
+        contentSize="lg"
+        style={{
+          backgroundImage: `url(${myProfileDesktopImg})`,
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: '90% 90%',
+          maxWidth: '1100px',
+          alignItems: 'center',
+        }}
       >
-        <strong>Seu nome</strong>
+        <UserAvatar
+          avatar={user?.avatar ?? null}
+          size="lg"
+          containButton
+          onUserAvatarButtonPress={handleUserAvatarChange}
+        />
+        <strong className="user-name">
+          {`${user?.firstName} ${user?.lastName}`}
+        </strong>
+        <span className="user-subject-name">
+          Geografia
+          {/* {user?.subject_name} */}
+        </span>
       </PageHeader>
     </div>
   );
