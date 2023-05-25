3b82e56e3faf6a81124917d2bd4e1d8e8cfa9d34
Marcelo Lupatini <mar.lupatini@gmail.com>
2023-05-25 17:39:35 -0300

    feat(web): atualização do formulário da interface GiveClasses

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,75 +1,103 @@
-import { useState, FormEvent } from 'react';
+import { useEffect, useState, FormEvent } from 'react';
 import { useHistory } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 import rocketIcon from '@assets/images/icons/rocket.svg';
 
+import { useAuth } from '@contexts/AuthContext';
+import { ApiClassSchedule, ClassSchedule, NotSavedClassSchedule } from '@dtos/ClassSchedule';
+import { Subject } from '@dtos/Subject';
 import api from '@services/api';
+import { createBlankClassSchedule } from '@utils/factories';
+import { parseFetchedToParsedClassSchedule } from '@utils/mappers';
 
+import { ClassScheduleForm } from '@components/ClassScheduleForm';
 import { EncouragementMessage } from '@components/EncouragementMessage';
 import { FormContainer } from '@components/FormContainer';
-import { FormFooter } from '@components/FormFooter';
 import { OuterLabelInput } from '@components/OuterLabelInput';
 import { PageHeader } from '@components/PageHeader';
 import { PageSubtitle } from '@components/PageSubtitle';
-import { Select } from '@components/Select';
+import { SubjectForm } from '@components/SubjectForm';
 import { Textarea } from '@components/Textarea';
+import { UserAvatar } from '@components/UserAvatar';
 
 import './styles.css';
 
 export function TeacherForm() {
+  const { fetchUser, user } = useAuth();
   const history = useHistory();
 
-  const [name, setName] = useState('');
-  const [avatar, setAvatar] = useState('');
-  const [whatsapp, setWhatsapp] = useState('');
-  const [bio, setBio] = useState('');
+  const firstName = user?.firstName ?? '';
+  const lastName = user?.lastName ?? '';
+  const avatar = user?.avatar ?? null;
+  const [whatsapp, setWhatsapp] = useState(user?.whatsapp ?? '');
+  const [bio, setBio] = useState(user?.bio ?? '');
 
-  const [subject, setSubject] = useState('');
+  const [subjects, setSubjects] = useState<Subject[]>([]);
+  const [subjectId, setSubjectId] = useState(-1);
   const [cost, setCost] = useState('');
 
-  const initialSchedule = { week_day: '', from: '', to: '' };
-  const [scheduleItems, setScheduleItems] = useState([initialSchedule]);
+  const [classSchedules, setClassSchedules] = useState<ClassSchedule[]>([]);
 
-  function addNewScheduleItem() {
-    setScheduleItems([...scheduleItems, initialSchedule]);
-  }
-
-  function setScheduleItemValue(
-    position: number,
-    field: string,
-    value: string
-  ) {
-    const updatedScheduleItems = scheduleItems.map((scheduleItem, index) => {
-      if (index === position) {
-        return { ...scheduleItem, [field]: value };
-      }
-
-      return scheduleItem;
-    });
-
-    setScheduleItems(updatedScheduleItems);
-  }
-
-  function handleCreateClass(e: FormEvent) {
+  async function handleEditUser(e: FormEvent) {
     e.preventDefault();
 
-    api.post('/classes', {
-      name,
-      avatar,
-      whatsapp,
-      bio,
-      subject,
-      cost: Number(cost),
-      schedule: scheduleItems,
-    }).then(() => {
-      alert('Cadastro realizado com sucesso');
+    try {
+      const parsedClassSchedules = classSchedules.map((classSchedule) => {
+        const parsedClassSchedule: NotSavedClassSchedule =
+          { ...classSchedule };
 
-      history.push('/');
-    }).catch((response) => {
-      alert('Erro no cadastro');
-      console.log(response);
-    });
+        if (parsedClassSchedule.id !== null && parsedClassSchedule.id < 0) {
+          parsedClassSchedule.id = null;
+        }
+
+        return parsedClassSchedule;
+      });
+
+      const data = {
+        whatsapp,
+        bio,
+
+        class: {
+          cost,
+          subject_id: subjectId,
+        },
+
+        class_schedules: parsedClassSchedules,
+      };
+
+      await api.put('/users/profile', data);
+
+      await fetchUser();
+
+      history.push('/confirmation', {
+        title: 'Cadastro salvo!',
+        message: 'Tudo certo, seu cadastro está na nossa lista de professores.<br />Agora é só ficar de olho no seu WhatsApp.',
+        buttonTitle: 'Acessar lista',
+        nextUri: '/study',
+      });
+    } catch (error) {
+      toast.error('Erro ao salvar perfil');
+    }
   }
+
+  useEffect(() => {
+    api.get('/classes/me').then((response) => {
+      const fetchedClassSchedules: ApiClassSchedule[] =
+        response.data.class_schedules;
+
+      const parsedClassSchedules = fetchedClassSchedules
+        .map(parseFetchedToParsedClassSchedule);
+
+      const newClassSchedules = parsedClassSchedules.length > 0
+        ? parsedClassSchedules
+        : [createBlankClassSchedule()];
+
+      setClassSchedules(newClassSchedules);
+      setSubjectId(response.data.class.subject.id);
+      setCost(response.data.class.cost);
+    });
+  }, []);
 
   return (
     <div id="page-teacher-form" className="page-container">
@@ -90,111 +118,47 @@ export function TeacherForm() {
         </div>
       </PageHeader>
 
-      <FormContainer handleSubmit={handleCreateClass}>
+      <FormContainer handleSubmit={handleEditUser}>
         <fieldset>
           <legend>Seus dados</legend>
 
-          <OuterLabelInput
-            name="name"
-            label="Nome completo"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-          <OuterLabelInput
-            name="avatar"
-            label="Avatar"
-            value={avatar}
-            onChange={(e) => setAvatar(e.target.value)}
-          />
-          <OuterLabelInput
-            name="whatsapp"
-            label="WhatsApp"
-            value={whatsapp}
-            onChange={(e) => setWhatsapp(e.target.value)}
-          />
+          <div className="personal-data-section">
+            <div className="main-personal-data">
+              <UserAvatar
+                avatar={avatar}
+                size="md"
+              />
+              <p>{`${firstName} ${lastName}`}</p>
+            </div>
+            <OuterLabelInput
+              name="whatsapp"
+              label="Whatsapp"
+              value={whatsapp}
+              onChange={(e) => setWhatsapp(e.target.value)}
+            />
+          </div>
           <Textarea
             name="bio"
             label="Biografia"
+            description="(Máximo 300 caracteres)"
             value={bio}
             onChange={(e) => setBio(e.target.value)}
           />
         </fieldset>
 
-        <fieldset>
-          <legend>Sobre a aula</legend>
+        <SubjectForm
+          subjects={subjects}
+          setSubjects={setSubjects}
+          subjectId={subjectId}
+          setSubjectId={setSubjectId}
+          cost={cost}
+          setCost={setCost}
+        />
 
-          <Select
-            name="subject"
-            label="Matéria"
-            value={subject}
-            onChange={(e) => setSubject(e.target.value)}
-            options={[
-              { value: 'Matemática', label: 'Matemática' },
-              { value: 'Português', label: 'Língua Portuguesa' },
-              { value: 'Biologia', label: 'Biologia' },
-              { value: 'Química', label: 'Química' },
-              { value: 'Física', label: 'Física' },
-              { value: 'Geografia', label: 'Geografia' },
-              { value: 'História', label: 'História' },
-              { value: 'Artes', label: 'Artes' },
-              { value: 'Educação Física', label: 'Educação Física' },
-            ]}
-          />
-          <OuterLabelInput
-            name="cost"
-            label="Custo da hora por aula"
-            value={cost}
-            onChange={(e) => setCost(e.target.value)}
-          />
-        </fieldset>
-
-        <fieldset>
-          <legend>
-            Horários disponíveis
-            <button type="button" onClick={addNewScheduleItem}>
-              + Novo horário
-            </button>
-          </legend>
-
-          { scheduleItems.map((scheduleItem, index) => (
-            <div
-              key={scheduleItem.week_day}
-              className="fields-section schedule-item"
-            >
-              <Select
-                name="week_day"
-                label="Dia da semana"
-                value={scheduleItem.week_day}
-                onChange={(e) => setScheduleItemValue(index, 'week_day', e.target.value)}
-                options={[
-                  { value: '0', label: 'Domingo' },
-                  { value: '1', label: 'Segunda-feira' },
-                  { value: '2', label: 'Terça-feira' },
-                  { value: '3', label: 'Quarta-feira' },
-                  { value: '4', label: 'Quinta-feira' },
-                  { value: '5', label: 'Sexta-feira' },
-                  { value: '6', label: 'Sábado' },
-                ]}
-              />
-              <OuterLabelInput
-                name="from"
-                label="Das"
-                type="time"
-                value={scheduleItem.from}
-                onChange={(e) => setScheduleItemValue(index, 'from', e.target.value)}
-              />
-              <OuterLabelInput
-                name="to"
-                label="Até"
-                type="time"
-                value={scheduleItem.to}
-                onChange={(e) => setScheduleItemValue(index, 'to', e.target.value)}
-              />
-            </div>
-          ))}
-        </fieldset>
-
-        <FormFooter />
+        <ClassScheduleForm
+          classSchedules={classSchedules}
+          setClassSchedules={setClassSchedules}
+        />
       </FormContainer>
     </div>
   );
