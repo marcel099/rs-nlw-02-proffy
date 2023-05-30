70ff6fb143994e872a4d0860770a0d6c84fd74e9
Marcelo Lupatini <mar.lupatini@gmail.com>
2023-05-30 12:31:48 -0300

    feat(mobile): componente de mensagem motivacional

---
+++ mobile/src/components/EncouragementMessage/styles.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  encouragementMessageContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  encouragementMessage: {
+    fontFamily: 'Poppins_400Regular',
+    color: '#D4C2FF',
+    fontSize: 12,
+
+    marginLeft: 8,
+  },
+});
