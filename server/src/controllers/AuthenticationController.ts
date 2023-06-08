6c2acaef835ec7e60c4b62ef6bd25552fcb18edb
Marcelo Lupatini <mar.lupatini@gmail.com>
2023-06-08 20:00:13 -0300

    style(server): correção de linting de AuthenticationController

--- server/src/controllers/AuthenticationController.ts
+++ server/src/controllers/AuthenticationController.ts
@@ -8,20 +8,17 @@ import { db } from '@database/connection';
 export class AuthenticationController {
   async session(request: Request, response: Response) {
     try {
-      const {
-        email,
-        password,
-      } = request.body;
+      const { email, password } = request.body;
 
       const userFound = await db('users')
         .where('email', '=', email)
         .select('id', 'password')
         .first();
 
-      if ( !userFound ) {
+      if (!userFound) {
         return response.status(400).json({
-          message: 'E-mail ou senha incorreta'
-        })
+          message: 'E-mail ou senha incorreta',
+        });
       }
 
       const hasPasswordMatched = await bcrypt.compare(
@@ -29,17 +26,16 @@ export class AuthenticationController {
         userFound.password
       );
 
-      if ( !hasPasswordMatched ) {
+      if (!hasPasswordMatched) {
         return response.status(400).json({
-          message: 'E-mail ou senha incorreta'
-        })
+          message: 'E-mail ou senha incorreta',
+        });
       }
 
       if (auth.token_secret === undefined) {
-        return response.status(500)
-          .json({
-            message: 'Não foi possível realizar login',
-          })
+        return response.status(500).json({
+          message: 'Não foi possível realizar login',
+        });
       }
 
       const token = jwt.sign({}, auth.token_secret, {
@@ -48,12 +44,10 @@ export class AuthenticationController {
       });
 
       return response.json({ token });
-    } catch ( err ) {
-      console.log(err)
-      return response.status(400)
-        .json({
-          message: 'E-mail ou senha incorreta'
-        })
+    } catch (error) {
+      return response.status(400).json({
+        message: 'E-mail ou senha incorreta',
+      });
     }
   }
 }
